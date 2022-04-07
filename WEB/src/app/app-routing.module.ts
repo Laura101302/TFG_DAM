55564e664b8b190a151bee644087098d29335768
Laura101302 <laurairene2002@gmail.com>
2022-04-07 21:30:56 +0200

    Página éxito

--- WEB/src/app/app-routing.module.ts
+++ WEB/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './components/error/error.component';
 import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { MensajeFinalComponent } from './components/mensaje-final/mensaje-final.component';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponentComponent } from './pages/home/home-component/home-component.component';
 import { ProductMayoristaComponent } from './pages/product-mayorista/product-mayorista.component';
@@ -19,6 +20,10 @@ const routes: Routes = [
   {
     path: 'mayoristas/productos/1',
     component: ProductMayoristaComponent,
+  },
+  {
+    path: 'exito',
+    component: MensajeFinalComponent,
   },
   {
     path: 'sobre-nosotros',
